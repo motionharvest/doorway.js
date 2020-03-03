ca1d9d55bcd01e7c511a411d45d0010a3c6d00a2
Aaron Sherrill <aaron@aaronsherrill.com>
2020-03-02 21:37:06 -0500

    Simplified implementation

--- public_html/src/js/doorway.js
+++ public_html/src/js/doorway.js
@@ -1,140 +1,120 @@
-(function(root, factory) {
-	'use strict';
 
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define([], factory);
-	}
-	else if (typeof exports === 'object') {
-		// COMMONJS
-		module.exports = factory();
-	}
-	else {
-		// BROWSER
-		root.Doorway = factory();
-	}
-}(this, function() {
-	'use strict';
 
-	function Doorway(elem, options){
-		var options = (options !== undefined) ? options : {
-				offset: 0
+document.Doorway = function(elem, options){
+	var options = (options !== undefined) ? options : {
+			offset: 0
+		},
+		mathutils = {
+			normalize: function($value, $min, $max) {
+				return ($value - $min) / ($max - $min);
 			},
-			mathutils = {
-				normalize: function($value, $min, $max) {
-					return ($value - $min) / ($max - $min);
-				},
 
-				interpolate: function($normValue, $min, $max) {
-					return $min + ($max - $min) * $normValue;
-				},
+			interpolate: function($normValue, $min, $max) {
+				return $min + ($max - $min) * $normValue;
+			},
 
-				map: function($value, $min1, $max1, $min2, $max2) {
+			map: function($value, $min1, $max1, $min2, $max2) {
 
-					if ($value < $min1) {
-						$value = $min1;
-					}
-
-					if ($value > $max1) {
-						$value = $max1;
-					}
-
-					var res = this.interpolate(this.normalize($value, $min1, $max1), $min2, $max2);
-					return res;
+				if ($value < $min1) {
+					$value = $min1;
 				}
-			};
 
-		var h = elem,
-			hi = h.outerHeight(),
-			scrollTop = 0,
-			delta = 0,
-			y = 0,
-			airborne = false,
-			pinTop = options.offset,
-			revealing = false,
-			revealed = true,
-			hiding = false,
-			hidden = false;
+				if ($value > $max1) {
+					$value = $max1;
+				}
 
-		$(document).on('touchmove scroll', function(evt) {
+				var res = this.interpolate(this.normalize($value, $min1, $max1), $min2, $max2);
+				return res;
+			}
+		};
 
-			//figure out how far we've scrolled since the last scroll event
-			delta = scrollTop;
-			scrollTop = $(window).scrollTop();
-			scrollTop = (scrollTop < 0)? 0 : scrollTop;
-			delta = scrollTop - delta;
+	var h = elem,
+		hi = h.outerHeight(),
+		scrollTop = 0,
+		delta = 0,
+		y = 0,
+		airborne = false,
+		pinTop = options.offset,
+		revealing = false,
+		revealed = true,
+		hiding = false,
+		hidden = false;
 
-			if (scrollTop > options.offset) {
-				airborne = true;
-			} else {
-				airborne = false;
-				pinTop = options.offset;
+	$(document).on('touchmove scroll', function(evt) {
+
+		//figure out how far we've scrolled since the last scroll event
+		delta = scrollTop;
+		scrollTop = $(window).scrollTop();
+		scrollTop = (scrollTop < 0)? 0 : scrollTop;
+		delta = scrollTop - delta;
+
+		if (scrollTop > options.offset) {
+			airborne = true;
+		} else {
+			airborne = false;
+			pinTop = options.offset;
+		}
+
+		if(airborne && delta > 0) {
+
+			if(revealed) {
+				revealed = false;
+				hiding = true;
+				pinTop = scrollTop;
 			}
 
-			if(airborne && delta > 0) {
-
-				if(revealed) {
-					revealed = false;
-					hiding = true;
-					pinTop = scrollTop;
+			if(hiding) {
+				y = mathutils.map(scrollTop, pinTop, pinTop + hi, 0, -hi);
+				if(y == -hi) {
+					hidden = true;
+					hiding = false;
+					console.log("hidden");
+					options.onUnpin();
 				}
-
-				if(hiding) {
-					y = mathutils.map(scrollTop, pinTop, pinTop + hi, 0, -hi);
-					if(y == -hi) {
-						hidden = true;
-						hiding = false;
-						console.log("hidden");
-						options.onUnpin();
-					}
-				}else if(revealing){
-					y = mathutils.map(scrollTop, pinTop - hi, pinTop, 0, -hi);
-					if(y == -hi) {
-						hidden = true;
-						hiding = false;
-						revealing = false;
-						console.log("hidden2");
-						options.onUnpin();
-					}
+			}else if(revealing){
+				y = mathutils.map(scrollTop, pinTop - hi, pinTop, 0, -hi);
+				if(y == -hi) {
+					hidden = true;
+					hiding = false;
+					revealing = false;
+					console.log("hidden2");
+					options.onUnpin();
 				}
-
-			} else if (delta < 0) {
-
-				if (hidden) {
-					revealing = true;
-					hidden = false;
-					pinTop = scrollTop;
-				}
-
-				if (revealing) {
-					y = mathutils.map(scrollTop, pinTop - hi, pinTop, 0, -hi);
-					if (y == 0) {
-						revealed = true;
-						revealing = false;
-						console.log("shown");
-						options.onPin();
-					}
-				}else if(hiding){
-					y = mathutils.map(scrollTop, pinTop, pinTop + hi, 0, -hi);
-					if (y == 0) {
-						revealed = true;
-						revealing = false;
-						hiding = false;
-						console.log("shown2");
-						options.onPin();
-					}
-				}
-
 			}
 
-			h.css("top", y);
+		} else if (delta < 0) {
 
-		});
-	}
+			if (hidden) {
+				revealing = true;
+				hidden = false;
+				pinTop = scrollTop;
+			}
 
+			if (revealing) {
+				y = mathutils.map(scrollTop, pinTop - hi, pinTop, 0, -hi);
+				if (y == 0) {
+					revealed = true;
+					revealing = false;
+					console.log("shown");
+					options.onPin();
+				}
+			}else if(hiding){
+				y = mathutils.map(scrollTop, pinTop, pinTop + hi, 0, -hi);
+				if (y == 0) {
+					revealed = true;
+					revealing = false;
+					hiding = false;
+					console.log("shown2");
+					options.onPin();
+				}
+			}
 
-	return Doorway;
-}));
+		}
+
+		h.css("top", y);
+
+	});
+}
 
 
 
